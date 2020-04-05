9fb18237cc147d5631d05810186cec7eb8de7ebb
Jasmin Rahimic <jasmin.rahimic@symphony.is>
2020-04-05 10:14:05 +0200

    new config

---
+++ init.el
@@ -0,0 +1,60 @@
+;; init.el --- Personal GNU Emacs configuration file.
+
+;; Copyright (c) 2019-2020 Protesilaos Stavrou <info@protesilaos.com>
+;;
+;; This file is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This file sets up the essentials for incorporating my init org
+;; file.  This is known as "literate programming", which I think is
+;; particularly helpful for sharing Emacs configurations with a wider
+;; audience that includes new or potential users (I am still very new
+;; myself).
+;;
+;; See my dotfiles: https://gitlab.com/protesilaos/dotfile
+
+;;; Code:
+
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+;; Initialise the packages, avoiding a re-initialisation.
+(unless (bound-and-true-p package--initialized)
+  (setq package-enable-at-startup nil)
+  (package-initialize))
+
+;; Make sure `use-package' is available.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Configure `use-package' prior to loading it.
+(eval-and-compile
+  (setq use-package-always-ensure nil)
+  (setq use-package-always-defer nil)
+  (setq use-package-always-demand nil)
+  (setq use-package-expand-minimally nil)
+  (setq use-package-enable-imenu-support t))
+
+(eval-when-compile
+  (require 'use-package))
+
+(require 'org)
+(setq vc-follow-symlinks t)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+;;; init.el ends here
